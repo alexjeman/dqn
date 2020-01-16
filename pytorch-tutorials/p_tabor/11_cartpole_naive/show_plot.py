ab800ca8ca363da35291423d02be2e88b8b530bd
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-16 09:33:36 +0200

    Move plot classes to show_plot.py

--- pytorch-tutorials/p_tabor/11_cartpole_naive/show_plot.py
+++ pytorch-tutorials/p_tabor/11_cartpole_naive/show_plot.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_learning_curve(x, scores, eps_history, filename):
+def plot_learning_curve(x, scores, epsilons, filename, lines=None):
     fig = plt.figure()
     ax = fig.add_subplot(111, label="1")
     ax2 = fig.add_subplot(111, label="2", frame_on=False)
 
-    ax.plot(x, eps_history, color="C0")
-    ax.set_xlabel("Training Stepts", color="C0")
+    ax.plot(x, epsilons, color="C0")
+    ax.set_xlabel("Training Steps", color="C0")
     ax.set_ylabel("Epsilon", color="C0")
     ax.tick_params(axis='x', colors="C0")
     ax.tick_params(axis='y', colors="C0")
@@ -15,13 +15,17 @@ def plot_learning_curve(x, scores, eps_history, filename):
     N = len(scores)
     running_avg = np.empty(N)
     for t in range(N):
-        running_avg[t] = np.mean(scores[max(0, t-100):(t+1)])
+        running_avg[t] = np.mean(scores[max(0, t - 20):(t + 1)])
 
     ax2.scatter(x, running_avg, color="C1")
     ax2.axes.get_xaxis().set_visible(False)
     ax2.yaxis.tick_right()
-    ax2.set_ylabel("Score", color="C1")
+    ax2.set_ylabel('Score', color="C1")
     ax2.yaxis.set_label_position('right')
-    ax2.tick_params(axis='y', color="C1")
+    ax2.tick_params(axis='y', colors="C1")
+
+    if lines is not None:
+        for line in lines:
+            plt.axvline(x=line)
 
     plt.savefig(filename)
