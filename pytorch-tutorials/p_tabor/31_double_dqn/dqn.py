a2d782c9bff12e2e2cfe77cc4331ef7aa8feed28
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-16 23:03:43 +0200

    Fork dqn from /17_dqn

---
+++ pytorch-tutorials/p_tabor/31_double_dqn/dqn.py
@@ -0,0 +1,52 @@
+import os
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+
+class DeepQNetwork(nn.Module):
+    def __init__(self, lr, n_actions, name, input_dims, chkpt_dir):
+        super(DeepQNetwork, self).__init__()
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
+
+        self.conv1 = nn.Conv2d(input_dims[0], 32, kernel_size=8, stride=4)  # input_dims the [0]'th element coresponds to the number of channels in the input image(in this case 4 by (1 gray scale image)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+
+        fc_input_dims = self.calculate_conv_output_dims(input_dims)
+
+        self.fc1 = nn.Linear(fc_input_dims, 512)
+        self.fc2 = nn.Linear(512, n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.SmoothL1Loss()
+        self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
+        self.to(self.device)
+
+    def calculate_conv_output_dims(self, input_dims):
+        state = T.zeros(1, *input_dims)
+        dims = self.conv1(state)
+        dims = self.conv2(dims)
+        dims = self.conv3(dims)
+        return int(np.prod(dims.size()))
+
+    def forward(self, state):
+        conv1 = F.relu(self.conv1(state))
+        conv2 = F.relu(self.conv2(conv1))
+        conv3 = F.relu(self.conv3(conv2))
+        conv_state = conv3.view(conv3.size()[0], -1)
+        flat1 = F.relu(self.fc1(conv_state))
+        actions = self.fc2(flat1)
+
+        return actions
+
+    def save_checkpoint(self):
+        print('Saving checkpoint...')
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print('Loading checkpoint...')
+        self.load_state_dict(T.load(self.checkpoint_file))
