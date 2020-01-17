0be75e4ea4cc74557338a8681b30b0d064651f1e
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-17 18:50:46 +0200

    Fork from /dueling_dqn

---
+++ pytorch-tutorials/p_tabor/36_dueling_double_dqn/dqn.py
@@ -0,0 +1,57 @@
+import os
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+
+class DuelingDeepQNetwork(nn.Module):
+    def __init__(self, lr, n_actions, name, input_dims, chkpt_dir):
+        super(DuelingDeepQNetwork, self).__init__()
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
+
+        self.conv1 = nn.Conv2d(input_dims[0], 32, kernel_size=8, stride=4)  # input_dims the [0]'th element coresponds to the number of channels in the input image(in this case 4 by (1 gray scale image)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+
+        fc_input_dims = self.calculate_conv_output_dims(input_dims)
+
+        self.fc1 = nn.Linear(fc_input_dims, 512)                            # Split in to 2 streams:
+        self.value_s = nn.Linear(512, 1)                                        # Value stream
+        self.action_s = nn.Linear(512, n_actions)                                # Action stream
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.SmoothL1Loss()
+        self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
+        self.to(self.device)
+
+    def calculate_conv_output_dims(self, input_dims):
+        state = T.zeros(1, *input_dims)
+        dims = self.conv1(state)
+        dims = self.conv2(dims)
+        dims = self.conv3(dims)
+        return int(np.prod(dims.size()))
+
+    def forward(self, state):
+        conv1 = self._swish(self.conv1(state))
+        conv2 = self._swish(self.conv2(conv1))
+        conv3 = self._swish(self.conv3(conv2))
+        conv_state = conv3.view(conv3.size()[0], -1)
+        flat1 = self._swish(self.fc1(conv_state))
+        value_s = self.value_s(flat1)
+        action_s = self.action_s(flat1)
+
+        return value_s, action_s
+
+    def _swish(self, x):
+        return x * T.sigmoid(x)
+
+    def save_checkpoint(self):
+        print('Saving checkpoint...')
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print('Loading checkpoint...')
+        self.load_state_dict(T.load(self.checkpoint_file))
